06901d659cb8a744db93ab1b421a0fb9626dfbed
Uddeshya Singh <106103684+jettspanner123@users.noreply.github.com>
2025-04-15 02:54:30 +0530

    date model changes

--- app/page.tsx
+++ app/page.tsx
@@ -3,10 +3,18 @@ import React from "react";
 import ApplicationThemeHelper, {ApplicationThemeStore} from "@/app/models/ThemeStore";
 import ApplicationNavigationBar from "@/app/components/ApplicationNavigationBar";
 import {ApplicationHelper, ApplicationHelperTypes} from "@/app/models/ApplicationHelper";
-import {BaseModel} from "@/app/models/DataModels/BaseModel"
 import {motion} from "motion/react";
 
 export default function Home(): React.JSX.Element {
+
+    ApplicationHelper.current.RenderEngin(() => {
+        (
+            async () => {
+                const locomotiveScroll = (await import("locomotive-scroll")).default;
+                const LocomotiveScroll = new locomotiveScroll();
+            }
+        )()
+    }, [])
 
     const {currentTheme, setTheme, toggleTheme} = ApplicationThemeStore();
     const applicationThemeHelper = new ApplicationThemeHelper(currentTheme, setTheme, toggleTheme);
@@ -14,6 +22,7 @@ export default function Home(): React.JSX.Element {
     const mousePosition: ApplicationHelperTypes.MousePosition = ApplicationHelper.current.getMousePosition();
     const transformedMousePosition: number = (mousePosition.x - ApplicationHelper.current.getWindowWidth() / 2);
 
+    const secondSectionReference: React.RefObject<HTMLElement | null> = React.useRef(null);
 
     return (
         <React.Fragment>
@@ -26,13 +35,13 @@ export default function Home(): React.JSX.Element {
             <main style={{
                 background: `radial-gradient(circle at calc(50% + ${transformedMousePosition / 4}px) 0%, #222222 0%, ${applicationThemeHelper.getBackgroundColor()} 100%)`,
                 color: applicationThemeHelper.getForegroundColor(),
-                paddingTop: ApplicationHelper.current.homeScreenVerticalPadding,
-            }} className={`min-h-screen max-h-screen w-screen flex flex-col items-center`}>
+            }} className={`min-h-[300vh] w-screen flex flex-col items-center`}>
+
 
                 <motion.section
                     style={{
                         width: ApplicationHelper.current.homeScreenElementWidth,
-                        marginTop: "5rem",
+                        paddingTop: "13rem",
                     }}
                     initial={{
                         filter: "blur(10px)",
@@ -48,42 +57,47 @@ export default function Home(): React.JSX.Element {
                         duration: 0.75,
                         delay: 0.5
                     }}
-                    className={`flex flex-col items-center justify-start`}>
 
-                    <h1 className={`font-bold text-white/80 text-[2rem] text-center`}>Your Complete Local Area
-                        Services</h1>
-                    <h3 style={{marginTop: "1rem"}}
-                        className={`font-semibold text-white/30 text-[1rem] text-center`}>Vercel provides the developer
-                        tools and cloud infrastructure</h3>
-                    <h3 className={`font-semibold text-white/30 text-[1rem] text-center`}>to build, scale, and secure a
-                        faster, more personalized web.</h3>
-
-
-                    <div style={{marginTop: "1rem"}} className={`flex gap-[1rem]`}>
-                        <ApplicationHomePageButton styles={{background: "white", color: "black"}}
-                                                   text={"Start Questionnaire"} action={() => {
-                        }}/>
-                        <ApplicationHomePageButton styles={{
-                            background: ApplicationHelper.current.royalGoldenGrey,
-                            border: "0.75px solid rgba(255,255,255,10)"
-                        }} text={"Log In / Sign Up"} action={() => {
-                        }}/>
-                    </div>
-
-                </motion.section>
-
-
-
-                <motion.section
-                    style={{
-                        width: "60%",
-                        height: "20rem",
-                        marginTop: "4rem",
-                    }}
-                    className={``}
+                    className={`h-screen w-screen`}
                 >
+                    <section
+
+                        className={`flex flex-col items-center justify-start`}>
+
+                        <h1 className={`font-bold text-white/80 text-[2rem] text-center`}>Your Complete Local Area
+                            Services</h1>
+                        <h3 style={{marginTop: "1rem"}}
+                            className={`font-semibold text-white/30 text-[1rem] text-center`}>Vercel provides the
+                            developer
+                            tools and cloud infrastructure</h3>
+                        <h3 className={`font-semibold text-white/30 text-[1rem] text-center`}>to build, scale, and
+                            secure a
+                            faster, more personalized web.</h3>
+
+
+                        <div style={{marginTop: "1rem"}} className={`flex gap-[1rem]`}>
+                            <ApplicationHomePageButton styles={{background: "white", color: "black"}}
+                                                       text={"Start Questionnaire"} action={() => {
+                            }}/>
+                            <ApplicationHomePageButton styles={{
+                                background: ApplicationHelper.current.royalGoldenGrey,
+                                border: "0.75px solid rgba(255,255,255,10)"
+                            }} text={"Log In / Sign Up"} action={() => {
+                            }}/>
+                        </div>
+
+                    </section>
 
                 </motion.section>
+
+
+
+
+                {/*second section*/}
+                <section ref={secondSectionReference} className={`h-screen w-screen bg-blue-300`}>
+
+                </section>
+
             </main>
         </React.Fragment>
     );
